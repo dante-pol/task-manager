7b49fb4d5b916b6125618e3dbbbc12667f4f697d
zeqipe <zeqipe@vk.com>
2024-02-07 02:31:44 +0300

    Предварительно созданы функции, реализующие контроллер

--- controller.py
+++ controller.py
@@ -3,22 +3,13 @@ import eel
 
 
 @eel.expose
-def get_one_task(id: int) -> str:
+def get_one_task(id: int) -> str or bool:
     """
     Вернет Task по ID, формат возврата <!--надо дописать--!>, тип данных str
     :param id: int - идентификатор Task
-    :return: str - Task
+    :return: str - Task, в случаи ошибки - вернет False
     """
-    pass
-
-
-@eel.expose
-def get_all_task() -> str:
-    """
-    Эта функция под вопросом.
-    :return:
-    """
-    pass
+    return WebPresenter.get_task(id)
 
 
 @eel.expose
@@ -33,7 +24,9 @@ def create_task(descript: str, start_time: str, deadline: str, is_done: bool = F
     :return: Вернет True, если задача успешно создана, либо False, если в процессе создания возникли какие либо ошибки,
              из-за которых задача не может быть создана.
     """
-    pass
+    result = TaskManager.create_task(descript, start_time, deadline, status=is_done)
+    last_task = TaskManager.get_count() - 1
+    return result if not result else get_one_task(last_task)
 
 
 
@@ -45,18 +38,19 @@ def remove_task(id: int) -> bool:
     :return: Вернет True, если задача успешно удалена, либо False, если в процессе удаления возникли какие либо ошибки,
              из-за которых задача не может быть удалена.
     """
-    pass
+    return TaskManager.delete_task(id)
 
 
 @eel.expose
-def switch_status(id: int) -> str:
+def switch_status(id: int) -> str or bool:
     """
     Меняет статус выполнения Task на противоположный. Вернет обновленную информацию о Task, формат возврата
     <!--надо дописать--!>, тип данных str
     :param id: int - идентификатор Task
-    :return: str - Task
+    :return: str - Task, в случаи ошибки - вернет False
     """
-    pass
+    TaskManager.set_status_task(id)
+    return get_one_task(id)
 
 
 @eel.expose
@@ -73,4 +67,4 @@ def add_time_for_deadline(id: str, time: str) -> bool:
     :return: Вернет True, если задача успешно отредактирована, либо False, если в процессе редкатирования возникли
              какие либо ошибки, из-за которых задача не может быть изменена.
     """
-    pass
+    return TaskManager.set_date_done(id, time)
